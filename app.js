28f443653275037cda10d855c0dc7d1ae249f3ac
VLADVASIAN-LAPT\VladV <vlad.vasian96@gmail.com>
2021-10-05 13:23:23 +0300

    Fixing issues & Refactoring

--- app.js
+++ app.js
@@ -2,15 +2,18 @@
 const burger = document.querySelector(".burger");
 const navbar = document.querySelector(".navigation-bar");
 const menuItems = document.querySelector(".navbar");
+const bodyul = document.querySelector("body");
 
 burger.addEventListener('click', () => {
 
     if ( navbar.classList.contains('clasa-test') ) {
         navbar.classList.remove('clasa-test');
         menuItems.classList.remove('clasa-test-2');
+        bodyul.style.overflow = "scroll";
     } else {
         navbar.classList.add('clasa-test');
         menuItems.classList.add('clasa-test-2');
+        bodyul.style.overflow = "hidden";
     }
 })
 
@@ -18,6 +21,7 @@ window.addEventListener("resize", function() {
     if (window.innerWidth > 700){
          navbar.classList.remove('clasa-test');
          menuItems.classList.remove('clasa-test-2');
+         bodyul.style.overflow = "scroll";
     }
   });
 
